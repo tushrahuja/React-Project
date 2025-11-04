d5ec602198df211250c4826f07cd2f5a6d299245
tushrahuja <ahuja.tushar.m@gmail.com>
2025-11-04 14:48:57 +0530

    add store and authSlice (redux part)

---
+++ src/store/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: false,
+  userData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    // these are reducer actions
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload.userData;
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userData = null;
+    },
+  },
+});
+
+//exporting each action of reducer
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
