f42c1beeb1e352d038f15d5ddd4b6f1bd389d64d
tushrahuja <ahuja.tushar.m@gmail.com>
2025-11-04 17:37:52 +0530

    more configurations of redux (in app.jsx mainly)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
-const App = () => { 
-  return (
-    <>
-    <h1>A Blog app with appwrite</h1>
-    </>
-  )
-}
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+};
+
+export default App;
